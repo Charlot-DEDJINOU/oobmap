8ac32859a49c1bb27a9f3a5141ae00ac0305358c
Charlot <dedjinoucharlotjoel@gmail.com>
2026-07-04 18:30:15 +0100

    refactor(transport): move transport.py into transport/ package as inject.py

---
+++ oobmap/transport/__init__.py
@@ -0,0 +1,29 @@
+from .inject import (
+    RawRequest,
+    InjectionPoint,
+    parse_raw_request,
+    split_cookie,
+    join_cookie,
+    with_header,
+    with_first_header,
+    inject,
+    current_value,
+    injection_points,
+    inject_marker,
+    send,
+)
+
+__all__ = [
+    "RawRequest",
+    "InjectionPoint",
+    "parse_raw_request",
+    "split_cookie",
+    "join_cookie",
+    "with_header",
+    "with_first_header",
+    "inject",
+    "current_value",
+    "injection_points",
+    "inject_marker",
+    "send",
+]
